efc61b2d58d0989247a1c9b3b7def2518ad38bdf
Ana-Maria Daneliuc <annouk.m@gmail.com>
2014-05-20 01:34:54 +0300

    em working version for 1D

--- Clusterization/clusterization/em.py
+++ Clusterization/clusterization/em.py
@@ -1,15 +1,17 @@
 import sys
+import getopt
 import random
 import numpy as np
 import utils as utils
 import scipy.stats as stats
 
 
-def estimation_step(K, means, points, stddev):
+def estimation(K, means, points, stddev):
     points_size = len(points)
     expectations = np.zeros((points_size, K))
     for i in range(0, points_size):
         total = np.zeros(2)
+        # total = 0
         current_point = points[i]
         for j in range(0, K):
             total += stats.norm(means[j], stddev).pdf(current_point)
@@ -19,11 +21,15 @@ def estimation_step(K, means, points, stddev):
     return expectations
 
 
-def maximization_step(K, expectations, means, points):
+def maximization(K, expectations, points):
     points_size = len(points)
+    means = []
     for j in range(0, K):
         m_step_numerator = np.zeros(2)
+        # m_step_numerator = 0
+
         m_step_denominator = np.zeros(2)
+        # m_step_denominator = 0
         for i in range(0, points_size):
             m_step_numerator += expectations[i][j] * points[i]
             m_step_denominator += expectations[i][j]
@@ -31,36 +37,59 @@ def maximization_step(K, expectations, means, points):
     return means
 
 
-def expectation_maximization(points, K, stddev):
-    # The initial means are random
+def expectation_maximization(points, K, stddev, means):
     old_means = []
-    means = random.sample(points, K)
-
 
     while not utils.convergence(means, old_means):
         old_means = means
 
         # the E step
-        expectations = estimation_step(K, means, points, stddev)
+        expectations = estimation(K, means, points, stddev)
         # the M step
-        means = maximization_step(K, expectations, old_means, points)
+        means = maximization(K, expectations, points)
 
-    return means, expectations # returns a tuple of them
+    return means, expectations  # returns a tuple of them
 
 
+error_msg = 'em.py -f <inputfile> -k <number of clusters> -m <comma-separated initial K means values> -s <stddev>'
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "f:k:m:s:", ["file=", "means=", "stddev="])
+except getopt.GetoptError:
+    print error_msg
+    sys.exit(2)
 
-# arg1: input filename
-# arg2: number of clusters
-input_filename = sys.argv[1]
-K = int(sys.argv[2])
+input_filename = None
+K = 0
+means = None
+stddev = None
+
+for opt, arg in opts:
+    if opt in ('-f', '--file'):
+        input_filename = arg
+    elif opt == '-k':
+        K = int(arg)
+    elif opt in ('-s', '--stddev'):
+        stddev = float(arg)
+    elif opt in ('-m', '--means'):
+        means_string = arg.split(',')
+        means = [float(m) for m in means_string]
+
+
+if input_filename is None or K == 0:
+    print error_msg
+    sys.exit(2)
 
 input_points = utils.read_points(input_filename)
-stddev = np.std(input_points, axis = 0)
 
-print input_points
-# clusterization = expectation_maximization(input_points, K, stddev)
+if stddev is None:
+    stddev = np.std(input_points, axis = 0)
 
-# print clusterization[0]
+if means is None:
+    means = random.sample(input_points, K)
+
+clusterization = expectation_maximization(input_points, K, stddev, means)
+
+print "centroids: {} \n expectations:\n {}".format(clusterization[0], clusterization[1])
 
 
 
