6169a8249ecca174be624e8b53fa7860ba1e01a3
anamariad <anna_m89@yahoo.com>
2014-06-13 19:26:26 +0300

    em2d

--- Clusterization/clusterization/em2d.py
+++ Clusterization/clusterization/em2d.py
@@ -32,6 +32,18 @@ def maximization(K, expectations, points):
     return means
 
 
+def q_function(K, stddev, points, centroids, expectations):
+
+    q = 0.0
+
+    for i in range(len(points)):
+        q += -np.log(K) + np.log(1.0 / np.sqrt(2 * np.pi * (stddev ** 2)))
+        for j in range(K):
+            q += -1.0 / (2 * stddev ** 2) * expectations[i][j] * ((np.linalg.norm(points[i] - centroids[j])) ** 2)
+
+    return q
+
+
 def expectation_maximization(points, K, stddev, means, threshold):
     old_means = np.zeros(means.shape)
     expectations = None
@@ -59,9 +71,10 @@ def assign_points_to_clusters(points, expectations, K):
         clusters[best_cluster_key].append(points[i])
     return clusters
 
-error_msg = 'em2d.py -f <inputfile> -k <number of clusters> -m <comma-separated initial K means values> -s <stddev> -t <threshold>'
+error_msg = 'em1d.py -i <inputfile> -k <number of clusters> -m <comma-separated initial K means values> ' \
+            '-s <stddev> -t <threshold> -o <outputfile>'
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "f:k:m:s:t:", ["file=", "means=", "stddev=", "threshold="])
+    opts, args = getopt.getopt(sys.argv[1:], "i:k:m:s:t:o:", ["inputfile=", "means=", "stddev=", "threshold=", "outputfile="])
 except getopt.GetoptError:
     print error_msg
     sys.exit(2)
@@ -70,11 +83,14 @@ input_filename = None
 K = 0
 means = None
 stddev = None
-threshold = 0.01
+threshold = None
+output_filename = None
 
 for opt, arg in opts:
-    if opt in ('-f', '--file'):
+    if opt in ('-i', '--inputfile'):
         input_filename = arg
+    elif opt in ('-o', '--outputfile'):
+        output_filename = arg
     elif opt == '-k':
         K = int(arg)
     elif opt in ('-s', '--stddev'):
@@ -92,6 +108,14 @@ if input_filename is None or K == 0:
     print error_msg
     sys.exit(2)
 
+if threshold is None:
+    threshold = 0.01
+
+if output_filename is None:
+    output_filename = "em.out"
+
+output_file = open(output_filename, 'w')
+
 input_points = utils.read_points(input_filename)
 
 if stddev is None:
@@ -100,9 +124,26 @@ if stddev is None:
 if means is None:
     means = np.array(random.sample(input_points, K))
 
+# writing the standard deviation to file
+output_file.write(str(stddev))
+output_file.write('\n')
+
 centroids, expectations, clusters = expectation_maximization(input_points, K, stddev, means, threshold)
 
 print "centroids:\n {} \n expectations:\n {}".format(centroids, expectations)
+
+
+# outputting q function to file
+output_file.write(str(q_function(K, stddev, input_points, centroids, expectations)))
+output_file.write('\n')
+
+# outputting centroids to file
+utils.print_array_to_file(centroids, output_file)
+
+# outputting expectations to file
+utils.print_matrix_to_file(expectations, output_file)
+
+output_file.close()
 
 utils.plot_clusters(centroids, clusters)
 
